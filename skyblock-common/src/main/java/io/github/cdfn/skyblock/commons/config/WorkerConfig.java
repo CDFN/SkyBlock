62d99a8d7582db51525ae5d1f0217f0a70403c37
CDFN <codefun@protonmail.com>
2021-04-21 01:05:14 +0200

    basic config for SWM

--- skyblock-common/src/main/java/io/github/cdfn/skyblock/commons/config/WorkerConfig.java
+++ skyblock-common/src/main/java/io/github/cdfn/skyblock/commons/config/WorkerConfig.java
@@ -1,7 +1,17 @@
 package io.github.cdfn.skyblock.commons.config;
 
 import eu.okaeri.configs.OkaeriConfig;
+import eu.okaeri.configs.annotation.Comment;
 
 public class WorkerConfig extends OkaeriConfig {
 
+  @Comment("How many worlds each worker should serve")
+  private Integer worldsLimit = 128;
+
+  @Comment("Loader for SWM (file, mysql, mongo, redis)")
+  private String loader = "redis";
+
+  public Integer getWorldsLimit() {
+    return worldsLimit;
+  }
 }
