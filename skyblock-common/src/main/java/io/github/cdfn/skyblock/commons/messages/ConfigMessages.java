ac74e0ae0c3268621df8634c1471964f055e020e
CDFN <codefun@protonmail.com>
2021-05-08 00:12:23 +0200

    switch to annotation based serializer

--- skyblock-common/src/main/java/io/github/cdfn/skyblock/commons/messages/ConfigMessages.java
+++ skyblock-common/src/main/java/io/github/cdfn/skyblock/commons/messages/ConfigMessages.java
@@ -1,32 +1,17 @@
 package io.github.cdfn.skyblock.commons.messages;
 
-import io.github.cdfn.skyblock.commons.messages.api.MessagePackSerializable;
-import java.io.IOException;
-import org.msgpack.core.MessagePack;
+import io.github.cdfn.skyblock.commons.messages.api.AnnotationMessageSerializer;
+import io.github.cdfn.skyblock.commons.messages.api.MessagePackField;
 
 public class ConfigMessages {
 
-  public static class ConfigRequest implements MessagePackSerializable {
+  public static class ConfigRequest implements AnnotationMessageSerializer {
 
-    private int id;
+    @MessagePackField
+    private Integer id;
 
     public ConfigRequest(int id) {
       this.id = id;
-    }
-
-    @Override
-    public byte[] serialize() throws IOException {
-      try(var packer = MessagePack.newDefaultBufferPacker()) {
-        packer.packInt(this.id);
-        return packer.toByteArray();
-      }
-    }
-
-    @Override
-    public void deserialize(byte[] bytes) throws IOException {
-      try(var unpacker = MessagePack.newDefaultUnpacker(bytes)) {
-        this.id = unpacker.unpackInt();
-      }
     }
 
     public int getId() {
@@ -34,31 +19,16 @@ public class ConfigMessages {
     }
   }
 
-  public static class ConfigResponse implements MessagePackSerializable {
+  public static class ConfigResponse implements AnnotationMessageSerializer {
 
-    private int id;
+    @MessagePackField
+    private Integer id;
+    @MessagePackField
     private String data;
 
     public ConfigResponse(int id, String data) {
       this.id = id;
       this.data = data;
-    }
-
-    @Override
-    public byte[] serialize() throws IOException {
-      try(var packer = MessagePack.newDefaultBufferPacker()) {
-        packer.packInt(this.id);
-        packer.packString(this.data);
-        return packer.toByteArray();
-      }
-    }
-
-    @Override
-    public void deserialize(byte[] bytes) throws IOException {
-      try(var unpacker = MessagePack.newDefaultUnpacker(bytes)) {
-        this.id = unpacker.unpackInt();
-        this.data = unpacker.unpackString();
-      }
     }
 
     public int getId() {
