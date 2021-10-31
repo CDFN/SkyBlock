b5d896a187c69ae3e5e117dc2dae2700577cfe86
cdfn <contact@cdfn.dev>
2021-10-31 17:36:30 +0100

    Use records for simple handlers

--- skyblock-velocity/src/main/java/io/github/cdfn/skyblock/message/handler/ConfigRequestHandler.java
+++ skyblock-velocity/src/main/java/io/github/cdfn/skyblock/message/handler/ConfigRequestHandler.java
@@ -7,18 +7,9 @@ import io.github.cdfn.skyblock.commons.message.ConfigMessages.ConfigResponse;
 import io.github.cdfn.skyblock.commons.message.api.MessagePublisher;
 import io.github.cdfn.skyblock.commons.message.api.handler.MessageHandler;
 import io.lettuce.core.RedisClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class ConfigRequestHandler implements MessageHandler<ConfigRequest> {
-  private final RedisClient client;
-  private final WorkerConfig workerConfig;
-
-  @Inject
-  public ConfigRequestHandler(RedisClient client, WorkerConfig workerConfig) {
-    this.client = client;
-    this.workerConfig = workerConfig;
-  }
+public record ConfigRequestHandler(@Inject RedisClient client, @Inject WorkerConfig workerConfig)
+    implements MessageHandler<ConfigRequest> {
 
   @Override
   public void accept(ConfigRequest configRequest) {
